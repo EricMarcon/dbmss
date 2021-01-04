222874684e68c796c359b0b38bd90e47a4b2d65c
Eric Marcon <e.marcon@free.fr>
2021-01-04 11:41:46 +0100

    spatstat.geom completes spatstat.core

--- R/as.Dtable.ppp.R
+++ R/as.Dtable.ppp.R
@@ -6,7 +6,7 @@ function (X, ...)
     X <- as.wmppp.ppp(X, ...)
   
   # Get the distance matrix
-  Dmatrix <- spatstat.core::pairdist(X)
+  Dmatrix <- spatstat.geom::pairdist(X)
   
   # Convert
   return (Dtable(Dmatrix, PointType=X$marks$PointType , PointWeight=X$marks$PointWeight))
