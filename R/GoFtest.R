ca15dc7d12fead3367a6bdf4590fd4077b91dd9e
Pie-Froment <pierrot.froment@gmail.com>
2025-03-28 16:32:02 -0300

    did minor modifications

--- R/GoFtest.R
+++ R/GoFtest.R
@@ -1,49 +1,3 @@
-GoFtest <- function(Envelope) {
-
-  # Verify Envelope
-  if (!inherits(Envelope, "envelope")) {
-    stop("Envelope is not of class envelope")
-  }
-  # Verify simulations
-  if (is.null(attr(Envelope, "simfuns"))) {
-    stop("Envelope does not contain simulations in its attribute simfuns")
-  } else {
-    r <- as.data.frame(attr(Envelope, "simfuns"))[, 1]
-    ActualValues <- Envelope$obs
-    SimulatedValues <- as.data.frame(attr(Envelope, "simfuns"))[, -1]
-  }
-
-  NumberOfSimulations <- dim(SimulatedValues)[2]
-  AverageSimulatedValues <- apply(SimulatedValues, 1, sum) / (NumberOfSimulations - 1)
-  rIncrements <- (r - c(0,r)[seq_along(r)])[-1]
-
-  # Ui calculate the statistic for a simulation
-  Ui <- function(SimulationNumber) {
-    Departure <- (SimulatedValues[, SimulationNumber] -
-      AverageSimulatedValues)[seq_along(r) - 1]
-    WeightedDeparture <- (Departure[!is.nan(Departure)])^2 *
-      rIncrements[!is.nan(Departure)]
-    return(sum(WeightedDeparture))
-  }
-
-  # Calculate the Ui statistic for all simulations
-  SimulatedU <- vapply(
-    seq_len(NumberOfSimulations),
-    FUN = Ui,
-    FUN.VALUE = 0
-  )
-
-  # Calculate the statistic for the actual value
-  RecenteredValues <- (ActualValues - AverageSimulatedValues)[seq_along(r) - 1]
-  WeightedRecenteredValues <- (RecenteredValues[!is.nan(RecenteredValues)])^2 *
-    rIncrements[!is.nan(RecenteredValues)]
-  ActualU <- sum(WeightedRecenteredValues)
-
-  # Return the rank
-  return(mean(ActualU < SimulatedU))
-}
-
-
 GoFtest <- function(
     Envelope,
     Test = "DCLF",
@@ -169,7 +123,7 @@ GoFtest <- function(
     ValueToTest = data.frame(ActualValues)
   )
 
-  # Return the p-value. If the p_value is equal to 0, a conservative p_value
+  # Return the p_value. If the p_value is equal to 0, a conservative p_value
   # of 1/(n + 1) is returned.
   return(ifelse(mean(ActualU < SimulatedU) == 0,
                 1 / (1 + NumberOfSimulations),
